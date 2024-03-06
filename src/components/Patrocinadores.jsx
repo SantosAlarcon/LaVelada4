c474864ae219b22298212aeb2780792a8f1ddc38
Santos Alarcón <santosalarcon86@gmail.com>
2024-03-06 15:46:44 +0100

    Añadido logo de Spotify

--- src/components/Patrocinadores.jsx
+++ src/components/Patrocinadores.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import Alsa from "./Patrocinadores/Alsa"
 
 const Patrocinadores = () => {
-	return (
-		<section className="p-4 flex gap-5 items-center justify-center">
-			<img id="patrocinadores" className="h-[48px] text-white" src="/patrocinadores/alsa.svg" alt="Alsa" />
-			<img className="h-[48px] text-white" src="/patrocinadores/revolut.svg" alt="Revolut" />
-			<img className="h-[48px] text-white" src="/patrocinadores/vicio.svg" alt="Vicio" />
-			<img className="h-[48px] text-white" src="/patrocinadores/prime.svg" alt="Prime" />
-			<img className="h-[48px] text-white" src="/patrocinadores/grefusa.svg" alt="Grefusa" />
-			<img className="h-[48px] text-white" src="/patrocinadores/cerave.svg" alt="CeraVe" />
-		</section>
-	)
+    return (
+        <section className="p-4 flex gap-5 items-center justify-center">
+            <Alsa className="h-[36px]" />
+            <img id="patrocinadores" className="h-[36px] text-white" src="/patrocinadores/revolut.svg" alt="Revolut" />
+            <img id="patrocinadores" className="h-[36px] text-white" src="/patrocinadores/vicio.svg" alt="Vicio" />
+            <img id="patrocinadores" className="h-[36px] text-white" src="/patrocinadores/prime.svg" alt="Prime" />
+            <img id="patrocinadores" className="h-[36px] text-white" src="/patrocinadores/grefusa.svg" alt="Grefusa" />
+            <img id="patrocinadores" className="h-[36px] text-white" src="/patrocinadores/cerave.svg" alt="CeraVe" />
+            <img id="patrocinadores" className="h-[36px] text-white" src="/patrocinadores/spotify.svg" alt="Spotify" />
+        </section>
+    )
 }
 
 export default Patrocinadores
